22af3f06e62c45d2ba6dfe4ba5fe477561fc5d05
fangorntheent <gus.lipkin@gmail.com>
2016-04-21 16:19:12 -0400

    Now less likely to be wrong

--- app/src/main/java/fangorntheent/honey/rpsdroid/WinChecker.java
+++ app/src/main/java/fangorntheent/honey/rpsdroid/WinChecker.java
@@ -9,41 +9,38 @@ public class WinChecker {
 
     public String winnerText;
     public int winnerInt;
+    public String winnerWord;
 
     public WinChecker() {
         winnerInt = -1;
         winnerText = "error";
+        winnerWord = "error";
     }
 
-    public void setWinner(int player, int ai) {
+    private void setWinnerVars(int player, int ai) {
         if ((player - ai == 1) || (player - ai == -2)) {
             winnerInt = 0;
             winnerText = "player";
+            winnerWord = "BEATS";
         }
         else if (player == ai){
             winnerInt = 1;
             winnerText = "tie";
+            winnerWord = "TIES WITH";
         }
         else {
             winnerInt = 2;
             winnerText = "ai";
+            winnerWord = "LOSES TO";
         }
     }
 
-    public void addWinner(int player, int ai, ArrayList<Integer> history) {
-        if ((player - ai == 1) || (player - ai == -2)) {
-            winnerInt = 0;
-            winnerText =  "player";
-        }
-        else if (player == ai){
-            winnerInt = 1;
-            winnerText = "tie";
-        }
-        else {
-            winnerInt = 2;
-            winnerText = "ai";
-        }
+    public void setWinner(int player, int ai) {
+        setWinnerVars(player, ai);
+    }
 
+    public void addWinner(int player, int ai, ArrayList<Integer> history) {
+        setWinnerVars(player, ai);
         history.add(winnerInt);
     }
 }
